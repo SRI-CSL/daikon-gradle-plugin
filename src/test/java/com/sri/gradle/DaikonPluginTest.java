4d4e013a113cc2d3930dd4b60fe9c8d56a395238
Michael Ernst <mernst@cs.washington.edu>
2020-10-03 13:47:31 -0700

    Enable GitHub Workflows CI and fix tests

--- src/test/java/com/sri/gradle/DaikonPluginTest.java
+++ src/test/java/com/sri/gradle/DaikonPluginTest.java
@@ -1,7 +1,7 @@
 package com.sri.gradle;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
 
 import com.sri.gradle.utils.Command;
 import com.sri.gradle.utils.Filefinder;
@@ -11,17 +11,19 @@ import java.util.List;
 import org.junit.Test;
 
 public class DaikonPluginTest {
-  @Test public void testJavafinder(){
+  @Test
+  public void testJavafinder() {
     Path dir = new File("src/main/java/com/sri/gradle/utils").toPath();
     System.out.println(dir);
     List<File> filesAvailable = Filefinder.findJavaFiles(dir);
-    assertThat(filesAvailable.size(), is(5));
+    assertThat("filesAvailable=" + filesAvailable, filesAvailable.size(), is(4));
   }
 
-  @Test public void testCommandBuilder(){
-    List<String> filesAvailable = Command.create().arguments("ls")
-        .permitNonZeroExitStatus().execute();
+  @Test
+  public void testCommandBuilder() {
+    List<String> filesAvailable =
+        Command.create().arguments("ls").permitNonZeroExitStatus().execute();
 
-    assertThat(filesAvailable.size(), is(9));
+    assertThat("filesAvailable=" + filesAvailable, filesAvailable.size(), is(9));
   }
 }
